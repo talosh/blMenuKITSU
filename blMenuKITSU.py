84a5f569834b0116618b9821b429e35d53011c68
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-05-28 18:11:38 +0200

    update .DS_Store, blMenuKITSU.py and app.py

--- blMenuKITSU.py
+++ blMenuKITSU.py
@@ -1,42 +1,18 @@
-import os
 import sys
-from PySide2 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget
 
-from pprint import pprint, pformat
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'My First Application'
+        self.initUI()
 
-from python.app import blMenuKITSU
-from python.framework import AppFramework
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(300, 300, 400, 200)  # position x, position y, width, height
+        self.show()
 
-
-'''
-from python.config import get_config_data
-from python.config import config_reader
-from python.tailon import tailon
-from python.metadata_fields import set_metadata_fields
-from python.sequence import sequence_sync
-from python.util import RobotLog
-from python.baselight import baselight_process
-'''
-
-APP_NAME = 'blMenuKITSU'
-DEBUG=True
-
-__version__ = 'v0.0.1.dev.001'
-
-def main():
-    fw = AppFramework(
-        app_name = APP_NAME,
-        version = __version__,
-        debug = DEBUG
-    )
-
-    app = blMenuKITSU([], framework = fw)
-    
-    window = app.main_window()
-    window.show()
-
-    app.exec_()
-    return app
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
