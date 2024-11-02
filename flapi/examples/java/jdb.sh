ab65601f678aa94ffbe84f84a3da513291e01a94
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-02 18:36:53 +0000

    delete 258 files update python.html and app.py

--- flapi/examples/java/jdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-/usr/bin/jdb \
-    -sourcepath "/Users/jamesm/filmlight/dev-jsonapi-18/build/baselight/Darwin-clang-800-64/debug/obj/flapi/java/sources:$(pwd)" \
-    -classpath "/Users/jamesm/filmlight/dev-jsonapi-18/build/baselight/Darwin-clang-800-64/debug/Utilities/Resources/share/flapi/java/*:$(pwd)" \
-    $*
--- flapi/examples/python/flimage.py
+++
@@ -1,9 +0,0 @@
-import sys
-import flapi
-
-conn = flapi.Connection("localhost")
-conn.connect()
-
-result = conn.FLImage.get_raw_metadata(sys.argv[1])
-
-print( result )
