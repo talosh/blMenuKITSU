dab509a9f1d2470898b0c2e1c80325596637da92
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-30 14:58:49 +0000

    update blMenuKITSU.py

--- blMenuKITSU.py
+++ blMenuKITSU.py
@@ -156,7 +156,7 @@ class FLAPIManager():
         if nshots > 0:
             shots = scene.get_shot_ids(0, nshots)
             for shot_ix, shot_inf in enumerate(shots):
-                print( "\r Querying Baselight metadata for shot %d of %s" % (shot_ix + 1, nshots), end="" )
+                # print( "\r Querying Baselight metadata for shot %d of %s" % (shot_ix + 1, nshots), end="" )
                 # log.verbose("Shot %d:" % shot_ix)
                 shot = scene.get_shot(shot_inf.ShotId)
                 shot_md = shot.get_metadata(md_keys)
@@ -184,7 +184,6 @@ class FLAPIManager():
                         'thumbnail_url': thumbnail_url
                     }
                 )
-
                 shot.release()
 
         return baselight_shots
